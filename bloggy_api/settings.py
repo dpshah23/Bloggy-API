22601634c2b6e4ef97bab8f464b1c9a753fa3e9e
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-08-20 08:02:39 +0530

    Added home app and configure urls

--- bloggy_api/settings.py
+++ bloggy_api/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'home',
+    'api',
+    'auth1',
     'corsheaders',
     'rest_framework',
     'django.contrib.admin',
--- bloggy_api/urls.py
+++ bloggy_api/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('auth1.urls')),
     path('api/', include('api.urls')),
+    path('/', include('home.urls')),
 ]
