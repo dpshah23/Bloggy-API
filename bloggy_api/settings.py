06bbbbb35e2feb2d80e033169a162a56dc09b0bb
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-08-21 11:09:10 +0530

    update

--- bloggy_api/settings.py
+++ bloggy_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(3=0b3339kl9ri4%ze$!9s84ncr05wvlq#nckqd6g1u4v2m*ou'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercle.app','localhost','*']
+ALLOWED_HOSTS = ['.vercel.app','*']
 
 
 # Application definition
