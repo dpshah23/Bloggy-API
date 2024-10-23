123aaf4c0f169f1daf4344a19da2af2a88a9e23e
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-10-23 13:11:35 +0530

    Added login logic

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('login', login, name='login'),
-    # path('logout', logout, name='logout'),
-    # path('dashboard', dashboard, name='dashboard'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+    path('dashboard/', dashboard, name='dashboard'),
 ]
--- adminpanel/views.py
+++ adminpanel/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Create your views here.
 
+def index(request):
+
+    if 'email' in request.session:
+        return render(request, 'dashboard.html')
+
+def dashboard(request):
+    
+        if 'email' in request.session:
+            return render(request, 'dashboard.html')
+        else:
+            return render(request, 'login.html')
+        
 def login(request):
+
+    if request.method=="POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        print(email, password)
+
+        print(os.getenv('EMAIL_ADMIN'), os.getenv('PASSWORD_ADMIN'))
+        if email==os.getenv('EMAIL_ADMIN') and password==os.getenv('PASSWORD_ADMIN'):
+            
+            request.session['email'] = email
+        
+            return redirect('/adminpanel/dashboard/')
+        
     return render(request, 'login.html')
+
+def logout(request):
+    request.session.flush()
+    return redirect('/adminpanel/login/')
